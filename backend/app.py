19819cc2c0ec1b002ef3157f8a3b2297107debab
RaymondHuang210129 <huan1763@purdue.edu>
2022-10-25 19:24:59 -0400

    fix: switch to newsdataapi for full news content

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,77 @@
 import json
+import os
 import sys
-from typing import Tuple
+from typing import Dict, Tuple
 
 import requests
 from flask import Flask, request
-
-from config import Config
+from newsdataapi import NewsDataApiClient
 
 sys.path.insert(0, "../news_traveler_document_similarity")
 sys.path.insert(0, "../news_traveler_sentiment_analysis")
 
-app = Flask(__name__)
-
 
 def send_newsapi_request(keyword: str) -> Tuple[int, str]:
-    params = {"q": keyword, "sortBy": "publishedAt", "apiKey": Config.newsapi_api_key}
-    response = requests.get(url=Config.newsapi_url, params=params, timeout=5)
+    api_key = os.getenv("NEWSAPI_KEY")
+    params = {"q": keyword, "sortBy": "publishedAt", "apiKey": api_key}
+    response = requests.get(
+        url="https://newsapi.org/v2/everything", params=params, timeout=5
+    )
     if response.ok:
         return response.status_code, response.content.decode("utf-8")
     return response.status_code, ""
+
+
+def parse_newsapi_response(newsapi_response: str) -> Tuple[str, int]:
+    newsapi_response = json.loads(newsapi_response)
+    response = {"totalResults": newsapi_response["totalResults"], "results": []}
+    for news in newsapi_response["articles"]:
+        response["results"].append(
+            {
+                "semanticInfo": {},
+                "source": news["source"],
+                "author": news["author"],
+                "title": news["title"],
+                "description": news["description"],
+                "url": news["url"],
+                "urlToImage": news["urlToImage"],
+                "publishedAt": news["publishedAt"],
+                "content": news["content"],
+            }
+        )
+    return json.dumps(response), 200
+
+
+def send_newsdataapi_request(keyword: str) -> Tuple[int, Dict]:
+    api_key = os.getenv("NEWSDATAAPI_KEY")
+    api = NewsDataApiClient(apikey=api_key)
+    print(api_key)
+    response = api.news_api(q=keyword, country="us")
+    if response["status"] != "success":
+        return 400, response
+    return 200, response
+
+
+def parse_newsdataapi_response(newsdataapi_response: Dict) -> Tuple[str, int]:
+    response = {"totalResults": newsdataapi_response["totalResults"], "results": []}
+    for news in newsdataapi_response["results"]:
+        response["results"].append(
+            {
+                "semanticInfo": {},
+                "source": news["source_id"],
+                "author": news["creator"],
+                "title": news["title"],
+                "description": news["description"],
+                "url": news["link"],
+                "urlToImage": news["image_url"],
+                "publishedAt": news["pubDate"],
+                "content": news["content"],
+            }
+        )
+    return json.dumps(response), 200
+
+
+app = Flask(__name__)
 
 
 @app.route("/search", methods=["GET"])
@@ -28,25 +81,11 @@ def search() -> str:
         if "keyword" not in request_content:
             raise ValueError
         keyword = request_content["keyword"]
-        status_code, newsapi_response = send_newsapi_request(keyword)
+        # status_code, response = send_newsapi_request(keyword)
+        status_code, response = send_newsdataapi_request(keyword)
         if status_code != 200:
             return f"newsAPI status_code {status_code}", 500
-        newsapi_response = json.loads(newsapi_response)
-        response = {"totalResults": newsapi_response["totalResults"], "results": []}
-        for news in newsapi_response["articles"]:
-            response["results"].append(
-                {
-                    "semanticInfo": {},
-                    "source": news["source"],
-                    "author": news["author"],
-                    "title": news["title"],
-                    "description": news["description"],
-                    "url": news["url"],
-                    "urlToImage": news["urlToImage"],
-                    "publishedAt": news["publishedAt"],
-                    "content": news["content"],
-                }
-            )
-        return json.dumps(response), 200
+        # return parse_newsapi_response(response)
+        return parse_newsdataapi_response(response)
     except ValueError:
         return 400, "Invalid json format"
--- backend/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    newsapi_url = "https://newsapi.org/v2/everything"
-    newsapi_api_key = "080bc0cc06ae48f9a1f7a4e532c80e5b"
